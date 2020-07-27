fb69489073aa94e26a62e11914af3ac5f169e4df
Randamus <rcviets@gmail.com>
2020-07-27 15:19:56 -0600

    mongodb atlas connect

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://rcviets:<Plzgtf0k!>@cluster0.76j9e.gcp.mongodb.net/<k4uClone>?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
+const client = new MongoClient(uri, { useUnifiedTopology: true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
   // perform actions on the collection object
   client.close();
 });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Connected correctly to server");
+
+    } catch (err) {
+        console.log(err.stack);
+    }
+    finally {
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
